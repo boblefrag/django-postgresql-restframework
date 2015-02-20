f575f39d9a16c57f4f9d2d7ef55633a589a0f88b
Yohann Gabory <yohann.gabory@novapost.net>
2015-02-20 23:17:44 +0100

    added hyperlinked serializatio

--- postgresql_restframework/serializers.py
+++ postgresql_restframework/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
-from rest_framework.relations import StringRelatedField
+from rest_framework.relations import (StringRelatedField,
+                                      HyperlinkedRelatedField)
 from django.db.models import F, Func
+from django.db.models import Value, CharField
+from django.db.models.functions import Concat
 
 
 class PostgreSQLListSerializer(serializers.ListSerializer):
@@ -15,13 +18,38 @@ class PostgreSQLModelSerializer(serializers.ModelSerializer):
 
 
 class PostgreSQLStringRelatedField(StringRelatedField):
-
+    """
+    Work exactly like StringRelatedField except it nicely create an
+    annotate on the list qs for postgresql json export
+    """
     def __init__(self, **kwargs):
         self.attr = kwargs.pop("attr")
-        print "POEUT"
         super(PostgreSQLStringRelatedField, self).__init__(**kwargs)
 
-    def postgresql_queryset(self, queryset):
+    def postgresql_queryset(self, queryset, **kwargs):
         f = "{}__{}".format(self.source, self.attr)
         kwargs = {self.source: F(f)}
+        return queryset.annotate(**kwargs)
+
+
+class PostgreSQLHyperLinkedField(HyperlinkedRelatedField):
+    """
+    Work exactly like StringRelatedField except it nicely create an
+    annotate on the list qs for postgresql json export
+    """
+    def __init__(self, **kwargs):
+        super(PostgreSQLHyperLinkedField, self).__init__(**kwargs)
+
+    def postgresql_queryset(self, queryset, request=None):
+        url = self.get_url(self.queryset.first(),
+                           self.view_name,
+                           request,
+                           format='json')
+        arg = getattr(self.queryset.first(), self.lookup_field)
+        url_list = [Value(t) for t in url.split(str(arg))]
+        f = F("{}__{}".format(self.source, self.lookup_field))
+        url_list.insert(1, f)
+        kwargs = {self.source: Concat(*url_list,
+                                      output_field=CharField())
+                  }
         return queryset.annotate(**kwargs)
