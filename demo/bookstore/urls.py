eb7c7266a377dce2a77d60fc8911ece3a67c4e2d
Yohann Gabory <yohann@gabory.fr>
2015-02-23 20:02:45 +0100

    added a detail url for editor

--- demo/bookstore/urls.py
+++ demo/bookstore/urls.py
@@ -5,6 +5,9 @@ from bookstore import views
 urlpatterns = [
     url(r'^books/$', views.BookList.as_view()),
     url(r'^books/(?P<pk>[0-9]+)$', views.BookDetail.as_view()),
+    url(r'^editors/(?P<pk>[0-9]+)$',
+        views.EditorDetail.as_view(),
+        name="editor-detail"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
